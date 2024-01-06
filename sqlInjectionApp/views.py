caa339d2469d9a8716c7fb4cf4d18349ae19ff28
jaeebawdekar <jaeebawdekar2511@gmail.com>
2024-01-06 15:52:13 +0530

    ctf challenge 1

--- sqlInjectionApp/views.py
+++ sqlInjectionApp/views.py
@@ -1,21 +1,30 @@
 from django.shortcuts import render
-from django .http import HttpResponse
+from django.http import HttpResponse
 from django.http import HttpResponseRedirect
-from . models import userLogin
-from . forms import loginForm
-
+from .models import userLogin
+from .forms import loginForm
+def is_admin_present(username):
+    return "admin" in username.lower()
 # Create your views here.
 def login(request):
     form = loginForm()
     if request.method == "POST":
         username = request.POST.get('username')
         password = request.POST.get('password')
-        print(username,password)
-        user = userLogin.objects.raw('SELECT * FROM sqlInjectionApp_userLogin WHERE username =  "{}" and password =  "{}"'.format(username,password))
-        print(user)
-        if user:
-            return HttpResponse("successfully logged in")
-        else:
-            return render(request,'sqlInjectionApp/login.html',{'form':form})
-    return render(request,'sqlInjectionApp/login.html',{'form':form})
+        print(username, password)
+
+        if  is_admin_present(username) and password:  # Check if password is not empty
+            # Simulate SQL injection by removing the password check using SQL comment sequence --
+            user_query = 'SELECT * FROM sqlInjectionApp_userLogin WHERE username = "{}" AND password = "{}"'.format(username, password)
+            user = userLogin.objects.raw(user_query)
+            
+
+            if user:
+                
+                return HttpResponse("Congratulations The flag is : LakshayCTF{$QLI_26}")
+        
+        return render(request, 'sqlInjectionApp/login.html', {'form': form, 'error_message': 'Login failed'})
+    
+    return render(request, 'sqlInjectionApp/login.html', {'form': form})
+
 
