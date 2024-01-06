90c247662c3c9bcd6bd1f13cec8b867a9fd4aab2
jaeebawdekar <jaeebawdekar2511@gmail.com>
2024-01-06 16:51:04 +0530

    changes for hosting purpose

--- djangoProject1/settings.py
+++ djangoProject1/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'w^=bl5j=ye7p1lw4kqffxj0ky8b^z@ro@$0xk(jun*m()7(imq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
